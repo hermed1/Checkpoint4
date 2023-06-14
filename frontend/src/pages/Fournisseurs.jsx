c7ed8d846039bfd4858bb1b09697420d9d1a1c56
Mehdi Charef <me-charef@hotmail.fr>
2023-06-14 18:15:50 +0200

    page done

--- frontend/src/pages/Fournisseurs.jsx
+++ frontend/src/pages/Fournisseurs.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useApi from "../services/useApi";
 
 function Fournisseurs() {
-  return <div>fournisseurs</div>;
+  const api = useApi();
+  const [fournisseursList, setFournisseursList] = useState([]);
+  useEffect(() => {
+    api
+      .get("/fournisseurs")
+      .then((resp) => {
+        setFournisseursList(resp.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      {fournisseursList.length > 0 ? (
+        fournisseursList.map((fournisseur) => (
+          <div key={fournisseur.id}>
+            <p>{fournisseur.nom}</p>
+            <img src={fournisseur.logo} alt="" />
+            <p>{fournisseur.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>problème de chargement</p>
+      )}
+    </div>
+  );
 }
 
 export default Fournisseurs;
